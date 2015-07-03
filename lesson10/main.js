e0ee71cb9324026bcc8e33d8c5ab619f0eeaaa1a
linyanfang <1399217809@qq.com>
2015-07-03 23:33:56 +0800

    fix 闭包

--- lesson10/main.js
+++ lesson10/main.js
@@ -25,3 +25,11 @@ for (var i = 0; i < 5; i++) {
     console.log(i);
   }, 5);
 }
+
+for (var i = 0; i < 5; i++) {
+  (function (idx) {
+    setTimeout(function () {
+      console.log(idx);
+    }, 5);
+  })(i);
+}
