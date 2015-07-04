80116b988595a15250b3e818f131af9badd64d31
linyanfang <1399217809@qq.com>
2015-07-04 22:50:11 +0800

    this对象未定义

--- lesson10/app.js
+++ lesson10/app.js
@@ -1,13 +1,26 @@
+//var myObject = {value: 100};
+//myObject.getValue = function () {
+//console.log(this.value);  // 输出 100
+
+//  // 输出 { value: 100, getValue: [Function] }，
+// // 其实就是 myObject 对象本身
+// console.log(this);
+
+//  return this.value;
+//};
+
+//console.log(myObject.getValue()); // => 100
+
 var myObject = {value: 100};
 myObject.getValue = function () {
-  console.log(this.value);  // 输出 100
+  var foo = function () {
+    console.log(this.value) // => undefined
+    console.log(this);// 输出全局对象 global
+  };
 
-  // 输出 { value: 100, getValue: [Function] }，
-  // 其实就是 myObject 对象本身
-  console.log(this);
+  foo();
 
   return this.value;
 };
 
 console.log(myObject.getValue()); // => 100
-
